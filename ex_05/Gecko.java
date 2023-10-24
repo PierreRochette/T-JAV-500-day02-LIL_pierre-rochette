02c7673b02fb3ad70a8fb135d1705b2813d33bfb
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-24 12:01:35 +0200

    update task5

--- ex_05/Gecko.java
+++ ex_05/Gecko.java
@@ -72,10 +72,5 @@ public class Gecko {
         }
     }
 
-    public static void main (String[] args) {
-        Gecko mimi = new Gecko("mimi");
-        mimi.hello("Titi");
-        mimi.hello(2);
-    }
 
 }
