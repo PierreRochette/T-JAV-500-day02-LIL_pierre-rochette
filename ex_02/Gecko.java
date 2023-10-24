f524d18449b6161dde9f39a7b58e3f52beb39514
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-24 11:03:20 +0200

    task 2

---
+++ ex_02/Gecko.java
@@ -0,0 +1,15 @@
+
+public class Gecko {
+
+    public String name;
+
+    public Gecko() {
+        this.name = "Unknown";
+        System.out.println("Hello!");
+    }
+
+    public Gecko(String name) {
+        this.name = name;
+        System.out.println("Hello " + this.name + "!");
+    }
+}
